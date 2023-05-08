0bff2e0369d8d5bc7bc62d4e8b988369cfcf86e1
ryuyal <helloliu777@163.com>
2023-05-08 12:14:44 +0800

    smart_pointer: shared_ptr

--- smart_pointer/smart_ptr.cpp
+++ smart_pointer/smart_ptr.cpp
@@ -3,6 +3,8 @@
 //
 
 #include<iostream>
+#include <memory>
+
 using namespace std;
 
 class Person{
@@ -18,7 +20,27 @@ public:
     }
 };
 
+void test(){
+    // 使用shared_ptr会自动调用Person的构造和析构函数
+    shared_ptr<Person> person = make_shared<Person>();
+    shared_ptr<Person> person2 = person;
+
+    // 查看person 和 person2的引用计数结果
+    cout << person.use_count() << endl;
+    cout << person2.use_count() << endl;
+}
 int main(){
     Person * p = new Person(); // 通过指针创建新的Person对象
     delete p; // 释放空间
+    cout << "---------------------" << endl;
+
+    /*
+     * 使用shared_ptr
+     */
+    shared_ptr<int> sp = make_shared<int>(10);
+    cout << *sp << endl;
+    cout << "---------------------" << endl;
+
+    test();
+
 }
